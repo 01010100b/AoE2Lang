c6503b08e8b9450903bb8c1ed7d5ee2a3802bce6
01010100b <caveman1917@hotmail.com>
2020-07-10 01:52:59 +0200

    m

---
+++ Compiler/Mods/EffectCommand.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Compiler.Mods
+{
+    
+
+    public abstract class EffectCommand
+    {
+        public static EffectCommand Get(YTY.AocDatLib.Effect effect)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class AttributeSetCommand : EffectCommand
+    {
+        public readonly int UnitId;
+        public readonly int ClassId;
+        public readonly Attribute Attribute;
+        public readonly float Amount;
+
+        public AttributeSetCommand(YTY.AocDatLib.Effect effect)
+        {
+            UnitId = effect.Arg1;
+            ClassId = effect.Arg2;
+            Attribute = (Attribute)effect.Arg3;
+            Amount = effect.Arg4;
+        }
+    }
+}
--- Compiler/Program.cs
+++ Compiler/Program.cs
@@ -1,7 +1,13 @@
-﻿using System;
+﻿using Compiler.Mods;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
+using System.Text;
 using System.Windows.Forms;
+using YTY.AocDatLib;
 
 namespace Compiler
 {
@@ -13,9 +19,30 @@ namespace Compiler
         [STAThread]
         static void Main()
         {
+            Test();
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+        }
+
+        internal static void Test()
+        {
+            Debug.WriteLine((int)Resource.SpiesDiscount);
+            var path = @"C:\Users\Tim\AppData\Roaming\Microsoft Games\Age of Empires ii\Data\Empires2_x1_p1.dat";
+            var dat = new DatFile(path);
+            
+            for (int i = 0; i < dat.Technologies.Count; i++)
+            {
+                var tech = dat.Technologies[i];
+
+                foreach (var effect in tech.Effects)
+                {
+                    if (effect.Command == 1)
+                    {
+                        //Debug.WriteLine("effect: " + i);
+                    }
+                }
+            }
         }
     }
 }
