bdb4475e356c8a6a440de47f9bff950795367889
01010100b <caveman1917@hotmail.com>
2020-08-09 16:01:38 +0200

    planner

---
+++ Algorithms/Planners/Action.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Algorithms.Planners
+{
+    public abstract class Action
+    {
+        public abstract IEnumerable<ResourceAmount> GetRequires(IEnumerable<ResourceAmount> state);
+        public abstract IEnumerable<ResourceAmount> GetBorrows(IEnumerable<ResourceAmount> state);
+        public abstract IEnumerable<ResourceAmount> GetConsumes(IEnumerable<ResourceAmount> state);
+        public abstract IEnumerable<ResourceAmount> GetProduces(IEnumerable<ResourceAmount> state);
+        public abstract TimeSpan GetDuration(IEnumerable<ResourceAmount> state);
+    }
+}
---
+++ Algorithms/Planners/ResourceAmount.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithms.Planners
+{
+    public struct ResourceAmount
+    {
+        public readonly int ResourceId;
+        public readonly int CurrentAmount;
+        public readonly int MinimumAmount;
+        public readonly int MaximumAmount;
+
+        public ResourceAmount(int id, int amount, int min, int max)
+        {
+            ResourceId = id;
+            CurrentAmount = amount;
+            MinimumAmount = min;
+            MaximumAmount = max;
+
+            if (min > max)
+            {
+                throw new ArgumentException("min can not be greater than max");
+            }
+
+            if (amount < min || amount > max)
+            {
+                throw new ArgumentException("current must be between min and max");
+            }
+        }
+
+        public ResourceAmount(int id, int amount) : this(id, amount, int.MinValue, int.MaxValue) { }
+    }
+}
